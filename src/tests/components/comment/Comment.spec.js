29c3d9aa5d5374aa43ea8f8711d9d26ca524cd67
Jingyu Feng <fengjingyu1995@gmail.com>
2020-03-01 01:01:26 -0500

    fixed test bug

--- src/tests/components/comment/Comment.spec.js
+++ src/tests/components/comment/Comment.spec.js
@@ -4,7 +4,19 @@ import { shallow } from 'enzyme';
 import { Comment, rootClass } from '../../../components/comment/Comment';
 
 describe('<Comment />', () => {
-  const wrapper = shallow(<Comment />);
+  const props = {
+    imgSrc: 'images/Avatar.jpg',
+    author: 'Brad',
+    occupation: 'AUTHOR',
+    timeAgo: '2 MINUTES AGO',
+    text: `So what the German automaker is likely to focus on today is the bigger
+    picture. This will be the first time we see the Taycan free from any
+    prototype bodywork.`,
+    numOfReply: 21,
+    numOfUpVote: 123,
+    numOfDownVote: 0
+  };
+  const wrapper = shallow(<Comment {...props} />);
 
   test('renders as expected', () => {
     expect(wrapper.find(`.${rootClass}`).exists()).toBeTruthy();
